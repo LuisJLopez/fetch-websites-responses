77ee68fe6fc8e4ac91f34704b8d78c3f4988ae01
Luis Lopez <Luisjlop@gmail.com>
2020-03-12 19:02:03 +0000

    New Constants file with a Set or URL's.

---
+++ project/constants.py
@@ -0,0 +1,10 @@
+URLS: set = {
+    'https://www.claranet.com',
+    'https://www.claranet.de',
+    'https://www.claranet.co.uk',
+    'https://www.claranet.fr',
+    'https://www.claranet.nl',
+    'https://www.claranet.es',
+    'https://www.claranet.pt',
+    'https://www.python.org'
+}
