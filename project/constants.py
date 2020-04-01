ad21454051a61b1adc0950440b82d318c417e697
Luis Lopez <Luisjlop@gmail.com>
2020-04-01 17:33:21 +0100

    Updated urls

--- project/constants.py
+++ project/constants.py
@@ -1,10 +1,10 @@
 URLS: set = {
     'https://www.claranet.com',
-    'https://www.twitter.com',
-    'https://www.facebook.com',
-    'https://www.google.fr',
-    'https://www.google.com',
-    'https://www.instagram.es',
+    'https://www.claranet.de',
+    'https://www.claranet.co.uk',
+    'https://www.claranet.fr',
+    'https://www.claranet.nl',
+    'https://www.claranet.es',
     'https://www.claranet.pt',
     'https://www.python.org'
 }
