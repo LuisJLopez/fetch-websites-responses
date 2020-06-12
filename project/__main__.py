c2fb8bc63ee20cdd24fbb263bf60df1a132c1c27
Luis Lopez <Luisjlop@gmail.com>
2020-06-12 22:16:07 +0100

    Update main with better indentation and proper loggin :rocket:

--- project/__main__.py
+++ project/__main__.py
@@ -1,32 +1,36 @@
 import aiohttp
 import asyncio
+import logging
 import time
 
-from bottle import Bottle, route, run, template
+from bottle import Bottle, run, template
 from constants import URLS
 from settings import DEBUG, HOST, PORT, RELOADER
 
 app = Bottle()
 
+
 async def get_request_data(url: str, session: aiohttp.ClientSession) -> dict:
-    print(f"Requesting {url}")
+    logging.info(f"Requesting {url}")
     start = time.time()
     try:
         response = await session.request(method="GET", url=url, timeout=10)
     except aiohttp.ClientConnectorError:
-        return {'url': url, 'status_code': response.status}
+        return {"url": url, "status_code": response.status}
     end = time.time()
     response_time: float = round(end - start, 4)
     html: str = await response.text()
-    print(f"Received response for {url} with status code: {response.status} in ~{response_time}s.")
+    logging.info(
+        f"Received response for {url} with status code: {response.status} in ~{response_time}s."
+    )
 
     return {
-        'url': url,
-        'method': response.method,
-        'html': html,
-        'status_code': response.status,
-        'response_time': response_time,
-        'size': response.content_length,
+        "url": url,
+        "method": response.method,
+        "html": html,
+        "status_code": response.status,
+        "response_time": response_time,
+        "size": response.content_length,
     }
 
 
@@ -40,13 +44,14 @@ async def get_website_analytics(urls: set) -> list:
         return await asyncio.gather(*tasks)
 
 
-@app.route('/', method='GET')
-@app.route('/<filter>', method='GET')
-def main(filter='response_time'):
+@app.route("/", method="GET")
+@app.route("/<filter>", method="GET")
+def main(filter="response_time"):
     data: list = asyncio.run(get_website_analytics(urls=URLS))
     sorted_data: list = sorted(data, key=lambda i: i[filter])
 
-    return template('project/views/index.tlp', data=sorted_data)
+    return template("project/views/index.tlp", data=sorted_data)
+
 
 run(app, host=HOST, port=PORT, debug=DEBUG, reloader=RELOADER)
 
